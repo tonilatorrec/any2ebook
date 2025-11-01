3ea8393586cf2bea5bf7093442207c6d9278a1f7
Toni Latorre <tonilatorre.c@gmail.com>
2025-11-01 18:13:39 +0100

    updated gitignore, added html2ebook, updated staging+convert script, can run main.py directly

--- any2ebook/main.py
+++ any2ebook/main.py
@@ -1,9 +1,15 @@
 import os
 import shutil
 
+import clippings_ingest
+import clippings_to_epub
+
 def main():
     if not os.path.exists('config.yaml'):
         shutil.copy('config_sample.yaml', 'config.yaml')
+    if not os.path.exists('./output'):
+        os.mkdir('./output')
 
 if __name__ == '__main__':
-    main()
+    clippings_ingest.main()
+    clippings_to_epub.main()
